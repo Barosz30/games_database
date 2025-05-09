c11f9e74021091dc6c3374ac9a5c0aff9a910374
Mirosław Wandyk <barosz1111@gmail.com>
2025-05-09 12:03:30 +0200

    first try on accessing global constans from expo

--- app/hooks/useIGDB.ts
+++ app/hooks/useIGDB.ts
@@ -1,10 +1,10 @@
 import { useToken } from "../context/tokenContext";
 import { fetchFromIGDB } from "../../services/api";
-
+import Constants from 'expo-constants';
 
 const useIGDB = () => {
   const { token, loading } = useToken();
-  const clientId = process.env.EXPO_PUBLIC_CLIENTID;
+  const clientId = process.env.EXPO_PUBLIC_CLIENTID || Constants.expoConfig?.extra?.EXPO_PUBLIC_CLIENTID;
 
   const fetchGames = (query: string, endpoint = "games") => {
     if (!token || !clientId) {
--- services/auth.ts
+++ services/auth.ts
@@ -1,6 +1,8 @@
+import Constants from 'expo-constants';
+
 export const getIGDBToken = async () => {
-    const clientId = process.env.EXPO_PUBLIC_CLIENTID
-    const clientSecret = process.env.EXPO_PUBLIC_SECRET
+  const clientId = process.env.EXPO_PUBLIC_CLIENTID || Constants.expoConfig?.extra?.EXPO_PUBLIC_CLIENTID;
+  const clientSecret = process.env.EXPO_PUBLIC_SECRET || Constants.expoConfig?.extra?.EXPO_PUBLIC_SECRET;
   
     const response = await fetch(
       `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`,
